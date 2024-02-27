57cf257a185ce9c0bff4554bf98ae43271504ae0
Ruben Hernandez <hernandzruben9@gmail.com>
2024-02-27 11:53:49 -0500

    h

--- src/AvlTree.java
+++ src/AvlTree.java
@@ -1,4 +1,12 @@
 import cu.edu.cujae.ceis.tree.binary.BinaryTree;
+import cu.edu.cujae.ceis.tree.binary.BinaryTreeNode;
 
-public class AvlTree<E> extends BinaryTree<E> {
+public class AvlTree<E extends Comparable<E> > extends BinaryTree<E> {
+
+    private class AvlNode<E> extends BinaryTreeNode<E> {
+        public E valor;
+        public int nivelSubarbol
+    }
+
+
 }
